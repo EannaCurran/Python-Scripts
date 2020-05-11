192de1690c17a0d9972706fe57d3265325f59519
Éanna Curran <emj.curran@gmail.com>
2020-05-11 18:41:01 +0100

    Completed sorting of files into folders

--- fileSorter.py
+++ fileSorter.py
@@ -1,1 +1,12 @@
-print("Hello world")
+
+
+import os
+import shutil
+
+files = [file for file in os.listdir() if os.path.isfile(file)]
+
+for file in files:
+    filename, extension = os.path.splitext(file)
+    if not os.path.exists(extension):
+        os.makedirs(extension)
+    shutil.move(file, extension)
