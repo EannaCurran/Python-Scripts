b7b684604c35c071ddeee7698112acd7812235fa
EannaCurran <emj.curran@gmail.com>
2020-07-14 13:53:46 +0100

    Get price

--- stockPrice.py
+++ stockPrice.py
@@ -4,5 +4,5 @@ import requests
 
 r = requests.get('https://finance.yahoo.com/quote/{}'.format(sys.argv[1]))
 soup = BeautifulSoup(r.content, parser='html-parser', features='lxml')
-list = soup.find(id='quote-header-info')
-print(list.prettify())
+price = soup.find("span", class_="Trsdu(0.3s)", attrs={"data-reactid": "14"}).get_text()
+print(price)
