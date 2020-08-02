1dc0293847a7284ce6a75731ffe95b2a827f8350
EannaCurran <emj.curran@gmail.com>
2020-08-02 16:43:17 +0100

    Prints all headlines found

--- .py/stockNews.py
+++ .py/stockNews.py
@@ -5,5 +5,8 @@ import requests
 try:
     r = requests.get('https://news.google.com/search?q={}+stock%20stock&hl=en-IE&gl=IE&ceid=IE%3Aen'.format(sys.argv[1]))
     soup = BeautifulSoup(r.content, features="lxml")
+    for headline in soup.find_all("h3"):
+        print(headline.text.strip())
+
 except Exception as e:
     print(e)
