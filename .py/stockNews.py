eb3bfdadccc38573df200d710b6d97f3acc5ae27
Éanna Curran <emj.curran@gmail.com>
2020-08-03 16:57:00 +0100

    Calculates sentiment of each headline using Textblob

--- .py/stockNews.py
+++ .py/stockNews.py
@@ -1,12 +1,17 @@
 import sys
 from bs4 import BeautifulSoup
 import requests
+from textblob import TextBlob
 
 try:
-    r = requests.get('https://news.google.com/search?q={}+stock%20stock&hl=en-IE&gl=IE&ceid=IE%3Aen'.format(sys.argv[1]))
+    r = requests.get(
+        'https://news.google.com/search?q={}+stock%20stock&hl=en-IE&gl=IE&ceid=IE%3Aen'.format(sys.argv[1]))
     soup = BeautifulSoup(r.content, features="lxml")
+    count = 0
+    total = 0
     for headline in soup.find_all("h3"):
-        print(headline.text.strip())
+        sentence = (headline.text.strip())
+        sentiment = TextBlob(sentence).sentiment.polarity
 
 except Exception as e:
     print(e)
